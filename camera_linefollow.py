e07e35c7681e30fe4069aa4c67b38b5d84839028
parayiln <parayiln@oregonstate.edu>
2022-01-28 20:58:28 -0800

    random error

--- camera_linefollow.py
+++ camera_linefollow.py
@@ -272,8 +272,8 @@ if __name__=='__main__':
 
 
     for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True): # use_video_port=True
-        frame = frame.array
-        lane.follow_lane(frame)
+        img = frame.array
+        lane.follow_lane(img)
         rawCapture.truncate(0)  # Release cache
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
