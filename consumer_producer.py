4252b6fba74292a3b3d25d396cb8d8cc6fd518ef
parayiln <parayiln@oregonstate.edu>
2022-02-04 15:52:55 -0800

    week 4

--- consumer_producer.py
+++ consumer_producer.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
 
     try:
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            print("enter")
             eSense = executor.submit(producer,.2, sense_bus, sense)
             eProcess = executor.submit(consumer_producer,.2, sense_bus, process_bus, process)
             eControl = executor.submit(consumer,.2, process_bus, control)
