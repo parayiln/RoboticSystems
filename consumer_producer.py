bec9a1ef81a6daeef96d26e30529ed4d129f58f4
parayiln <parayiln@oregonstate.edu>
2022-02-04 19:17:07 -0800

    week 4

--- consumer_producer.py
+++ consumer_producer.py
@@ -18,7 +18,7 @@ def consumer_producer(sense_bus, process_bus, delay):
     infer = Interpretation()
     while True:
         data_read_cp = sense_bus.read()
-        data_pocess_cp = infer.Processing(data_read)
+        data_pocess_cp = infer.Processing([data_read[0],1,1])
         print(type(data_process_cp))
         process_bus.write(data_process_cp)
         time.sleep(delay)
