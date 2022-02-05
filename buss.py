28b3d39b05c8d82b887e95fdde894e8c67979186
parayiln <parayiln@oregonstate.edu>
2022-02-04 18:00:05 -0800

    week 4

--- buss.py
+++ buss.py
@@ -3,6 +3,8 @@
 import sys
 sys.path.append(r'/home/nidhi/.local/lib/python3.7/site-packages')
 from readerwriterlock import rwlock
+
+
 class buss(object):
     def __init__(self,message):
         self.lock = rwlock.RWLockWriteD ()
@@ -11,6 +13,7 @@ class buss(object):
     def write(self,message):
         with self.lock.gen_wlock ():
             self.message = message
+            print(message)
 
     def read(self):
         with self.lock.gen_rlock ():
