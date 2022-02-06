6a65eb6331fb34db2276bdbf03801751fc5b79af
parayiln <parayiln@oregonstate.edu>
2022-02-05 21:28:00 -0800

    adding week 5

---
+++ concurrent.py
@@ -0,0 +1,40 @@
+# week 5
+
+import sys
+sys.path.append(r'/home/nidhi/RoboticSystems/lib/')
+from sensing_control import Sensing, Interpretation, Controller
+import rossros as ros
+from sensing_control_ultrsonic import SensingUltra, InterpretationUltra, ControllerUltra
+
+
+
+if __name__=='__main__':
+    Sensor = Sensing()
+    SensorU= SensingUltra()
+    Process = Interpretation()
+    ProcessU = InterpretationUltra()
+    Control = Controller()
+    ControlU = ControllerUltra()
+
+    sense_bus=ros.Bus()
+    process_bus=ros.Bus()
+    senseU_bus=ros.Bus()
+    processU_bus=ros.Bus()
+    termination_bus = ros.Bus()
+
+    delay=.01
+
+    sense_p = ros.Producer(Sensor.sensing, sense_bus, delay, termination_bus, 'sense line')
+    process_cp = ros.ConsumerProducer(Process.processing, sense_bus, process_bus, delay, termination_bus, 'process line')
+    control_c= ros.Consumer(Control.move, process_bus, delay, termination_bus, 'control line follower')
+
+    sense_u_p = ros.Producer(SensorU.sensing, senseU_bus, delay, termination_bus, 'sense object')
+    process_u_cp = ros.ConsumerProducer(ProcessU.processing, senseU_bus, processU_bus, delay, termination_bus, 'process object')
+    control_u_c = ros.Consumer(ControlU.stop_move, processU_bus, delay, termination_bus, 'stop if object')
+
+    time = ros.Timer(termination_bus, 5, 0.01, term_bus, "Timer")
+
+    try:
+        ros.runConcurrently([sense_p, sense_u_p, process_cp, process_u_cp, control_c, control_u_c, time.timer()])
+    except:
+        print("Someting is wrong, exiting code")
---
+++ sensing_control_ultasonic.py
@@ -0,0 +1,88 @@
+# week 5
+
+import sys
+import atexit
+sys.path.append(r'/home/nidhi/RoboticSystems/lib')
+from picarx_improved import Picarx
+import time
+import numpy
+from ultrasonic import Ultrasonic
+
+class Sensing(object):
+    def __init__(self, trig_pin = Pin("D2") , echo_pin = Pin("D3"), timeout=0.02):
+        self.trig = trig
+        self.echo = echo
+        self.timeout = timeout
+
+    def sensing(self):
+        ultra_sonic = Ultrasonic()
+        dist_value=ultra_sonic.read()
+        return dist_value
+
+class Interpretation(object):
+    # take arguments for sensitivity and polarity
+    def __init__(self, ref_TooClose = 25):
+        self.TooClose = ref_TooClose
+
+
+    def caliberation(self,cali):
+        cali_val = cali
+        sum=0
+        for i in range(len(cali)):
+            sum = sum+cali[i]
+        final_cali = sum/len(cali)
+        self.TooClose = final_cali
+
+    def processing(self,data):
+        val=data
+        close=1 #defalut not close
+        if val > self.TooClose:
+            close=1
+        else:
+            close=0
+        return close
+
+
+class Controller(object):
+    def __init__(self):
+        self.sense=Sensing()
+        # self.infer = Interpretation(self.sense)
+        self.motor =motor_command.Picarx()
+
+    def stop_move(self,dist):
+        close=dist
+        if close==0:
+            self.motor.stop()
+            time.sleep(0.01)
+        else:
+            self.motor.forward(30)
+            time.sleep(0.01)
+
+    def stop(self):
+        self.motor.stop()
+        time.sleep(0.01)
+
+ # automatic steering
+
+if __name__=='__main__':
+    sense = Sensing(500)
+    print("Code for line following using the Grayscale module")
+    data=sense.sensing()
+
+    infer=Interpretation()
+    dist=infer.processing(data)
+    control= Controller()
+
+    print("Select 2 to calibrate or 1 to use the defalut values ")
+    flag_cali=input()
+    if flag_cali==2:
+        print("please hold against the wall at a safe distance ")
+        cali=[]
+        cali.append(sense.sensing())
+        cali.append(sense.sensing())
+        cali.append(sense.sensing())
+    else:
+        if flag_cali != 1 and flag_cali != 2:
+            print("no valid selection made using the defaut values")
+        while (True):
+            control.stop_move(dist)
